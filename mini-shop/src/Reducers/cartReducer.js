0d8df91c60496610a373dc333245a785da3ebbc3
Elham <elham.yadegary@gmail.com>
2021-11-07 20:22:40 +0330

    last commit

--- mini-shop/src/Reducers/cartReducer.js
+++ mini-shop/src/Reducers/cartReducer.js
@@ -8,12 +8,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
         case CART_ADD_ITEM:
             const item = action.payload
             const existItem = state.cartItems.find((x) => x.id === item.id)
-
             if (existItem) {
                 return {
                     ...state,
                     cartItems: state.cartItems.map(x =>
-                        x.id === existItem.id ? item : x)
+                        x.id === existItem.id ? item : x),
                 }
             } else {
                 return {
